3b7c816293c82670fef33061c8e2252289b9d2b7
chic-caller-7520 <arbrown110@gmail.com>
2020-04-03 22:55:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,25 +1,8 @@
 def third_challenge
     epic_tragedy = {
-     :montague => {
-        :patriarch => { name : "Lord Montague" , age :"53"
-         
-          },
-        :matriarch => { name : "Lady Montague" , age :"54"
-         
-          },
-        :hero => { name : "Romeo" , age :"15", status : "alive"
-          },
-        :heroine_friends => []
+     :montague => {:patriarch => { name : "Lord Montague" , age :"53"},:matriarch => { name : "Lady Montague" , age :"54"},:hero => { name : "Romeo" , age :"15", status : "alive"},:heroine_friends => []
       
-     :capulet => {
-        :patriarch => { name : "Lord Capulet" , age :"53"
-          
-          },
-        :matriarch => { name : "Lady Capulet" , age :"51"
-        
-          },
-        :heroine => { name : "Juliet" , age :"15", status : "alive" },
-        :heroine_friends => []
+     :capulet => { :patriarch => { name : "Lord Capulet" , age :"53"}, :matriarch => { name : "Lady Capulet" , age :"51"}, :heroine => { name : "Juliet" , age :"15", status : "alive" },:heroine_friends => []
      }
     }
   end
