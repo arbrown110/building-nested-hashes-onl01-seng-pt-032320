9d9be526586e386642b85c8edb8a30931b23c1dd
chic-caller-7520 <arbrown110@gmail.com>
2020-04-03 23:07:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,8 +10,8 @@ def third_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliette", age: "15", status: "alive"},
-      :heroine_friends => []
+      :hero=> {name: "Juliette", age: "15", status: "alive"},
+      :hero_friends => []
    }
   }
 end
